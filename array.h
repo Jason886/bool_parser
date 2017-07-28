3a8be72314e4a0a52a51b9a23f8186342ba0f1dd
jason886 <359152155@qq.com>
2017-07-28 17:47:23 +0800

    modi

--- array.h
+++ array.h
@@ -38,6 +38,32 @@ void array_clear(array_t *arr);
 void array_foreach(array_t *arr, array_foreach_callback cb);
 void array_foreach_reverse(array_t *arr, array_foreach_callback cb);
 
+#define arr_init(T, arr) array_init(arr, sizeof(T))
+#define arr_uinit(arr) array_uinit(arr)
+#define arr_cap(arr) array_capacity(arr)
+#define arr_n(arr) array_size(arr)
+#define arr_ele_size(arr) array_element_size(arr)
+#define arr_empty(arr) array_empty(arr)
+#define arr_shrk(arr) array_shrink(arr)
+#define arr_append(arr, e) \
+(T ee = (T)(e), array_push_back(arr, &ee))
+#define arr_insert(arr, idx, e) \
+(T ee = (T)(e), array_insert(arr, &ee, idx))
+#define arr_append_r(arr, pe) array_push_back(arr, pe)
+#define arr_insert_r(arr, idx, pe) array_insert(arr, pe, idx)
+#define arr_rm_last(arr) array_pop_back(arr)
+#define arr_get(T, arr, idx) \
+(T e, array_at(arr, idx, &e), &e)
+#define arr_get_ref(T, arr, idx) \
+(T *pe =0, array_ref_at(arr, idx, &pe), pe)
+#define arr_last(T, arr) \
+(T e, array_back(arr, &e), e)
+#define arr_last_ref(T, arr) \
+{} \
+#define arr_remove(arr, idx) array_erase(arr, idx)
+#define arr_clear(arr) array_clear(arr, idx)
+#define arr_for(arr, cb) array_foreach(arr, (array_foreach_callback)(cb))
+#define arr_rfor(arr, cb) array_foreach_reverse(arr, (array_foreach_callback)(cb));
 
 #define ARRAY_DEFINE(T, pre) \
 static int pre##_array_init(array_t *arr) { \
@@ -53,6 +79,7 @@ static int pre##_array_insert(array_t *arr, T e, size_t idx) { \
 	return array_insert(arr, &e, idx); \
 } \
 typedef void (* pre##_array_foreach_callback)(array_t *arr, T *p_ele, size_t idx);\
+typedef pre##_array_foreach_callback pre##_arr_for_cb; \
 static void pre##_array_foreach(array_t * arr, pre##_array_foreach_callback cb) { \
 	array_foreach(arr, (array_foreach_callback) cb); \
 } \
